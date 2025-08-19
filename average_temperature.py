c9e0ade6bcdab4428729967cf450acc67fc1df92
azrapatvi <azrapatvi@gmail.com>
2025-08-20 00:18:37 +0530

    Create average_temperature.py

---
+++ average_temperature.py
@@ -0,0 +1,11 @@
+n=int(input("enter length:"))
+l1=[]
+for i in range(n):
+    i=int(input("enter:"))
+    l1.append(i)
+
+avearge_of_temperatures=sum(l1)/len(l1)
+print("average of temperatures:",avearge_of_temperatures)
+
+
+    
