8a7b4914498de2ad37873d0d705e8bb1cd242eaa
azrapatvi <azrapatvi@gmail.com>
2025-08-27 18:53:40 +0530

    Create array_ex.py

---
+++ array_ex.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+arr1=np.array([1,2,3,4,5,6])
+arr2=np.array([2,4,6,8,10,12])
+
+print("addition:",arr1+arr2)
+print("mean:",arr1.mean())
+print("2d array:",arr1.reshape(2,3))
+
+
+
+
+
+
